9b3358d97403a0ab080c0bdf6ddd7e81ebbdfc8c
Thiago Petrimperni <thiago.srpt@gmail.com>
2023-02-11 22:49:21 -0500

    chore: udpates and api

--- index.js
+++ index.js
@@ -1,33 +1,59 @@
+var express = require('express');
+var app     = express();
+var cors    = require('cors');
+var dal    = require('./dal');
 
-function Spa() {
-  return (
-    <HashRouter>
-      <NavBar/>
-      <UserContext.Provider value={{users:[
-        {name:'abel',email:'abel@mit.edu',password:'secret',balance:100,auth:"false"},
-        {name:'nick',email:'nick@mit.edu',password:'secret',balance:60,auth:"false"},
-        {name:'Will',email:'william@mit.edu',password:'secret',balance:100,auth:"false"}
-        ]}}>
-        <div className="container" style={{padding: "20px"}}>
-          <Route path="/" exact component={Home} />
-          <Route path="/CreateAccount/" component={CreateAccount} />
-          <Route path="/login/" component={Login} />
-          <Route path="/deposit/" component={Deposit} />
-          <Route path="/withdraw/" component={Withdraw} />
-          <Route path="/balance/" component={Balance} />
-          <Route path="/alldata/" component={AllData} />
-        </div>
-      </UserContext.Provider>      
-    </HashRouter>
-  );
-}
-
-ReactDOM.render(
-  <Spa/>,
-  document.getElementById('root')
-);
+app.use(express.static('public'));
+app.use(cors());
 
 
+//create account
+app.get('/account/create/:name/:email/:password', function (req, res) {
+    dal.create(req.params.name,req.params.email,req.params.password).
+        then((user) => {
+            console.log(user);
+            res.send(user);
+        });
+});
 
 
+//  delete by email
+app.get('/account/delete/:email', function (req, res) {
 
+    dal.deleteByEmail().
+        then((docs) => {
+            console.log(docs);
+            res.send(docs);
+        });
+});
+
+app.get('/accounts/delete', function (req, res) {
+
+    dal.deleteAll().
+        then((docs) => {
+            console.log(docs);
+            res.send(docs);
+        });
+});
+
+// get all accounts
+app.get('/accounts/all', function (req, res) {
+
+    dal.all().
+        then((docs) => {
+            console.log(docs);
+            res.send(docs);
+        });
+});
+
+app.get('/account/update/:email/:balance', function (req, res) {
+    dal.update(req.params.email, req.params.balance)
+        .then((user) => {
+            console.log(user);
+            res.send(user);
+        });
+});
+
+
+app.listen(3000);
+console.log('Running on port: 3000')
